37bae0995c80a504b2498250a78711ca56722331
jethro0610 <jethro0610@outlook.com>
2024-02-27 22:35:10 -0800

    Simplify choropleth

--- index.js
+++ index.js
@@ -9,49 +9,21 @@ function testGraph() {
         .attr("width", width)
         .attr("height", height);
 
-    const path = d3.geoPath();
     const projection = d3.geoMercator()
         .scale(70)
         .center([0, 20])
         .translate([width / 2, height / 2]);
 
-    const data = d3.map();
-    const colorScale = d3.scaleThreshold()
-        .domain([
-            100000, 
-            1000000, 
-            10000000, 
-            30000000, 
-            100000000, 
-            500000000
-        ])
-        .range(d3.schemeBlues[7]);
-
-    d3.queue()
-        .defer(
-            d3.json, 
-            "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-        )
-        .defer(
-            d3.csv, 
-            "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv",
-            function(d) {
-                data.set(d.code, +d.pop);
-            }
-        )
-        .await(ready);
-
-    function ready(error, topo) {
+    d3.json("world.json", function(data) {
         svg.append("g")
             .selectAll("path")
-            .data(topo.features)
-            .enter()
-            .append("path")
+            .data(data.features)
+            .enter().append("path")
+                .attr("fill", "gray")
                 .attr("d", d3.geoPath()
                     .projection(projection)
                 )
-                .attr("fill", "blue")
-    }
+    })
 }
 
 testGraph();
