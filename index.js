acb4202e7ce669b762935cc4979f5d64a8a5a687
jethro0610 <jethro0610@outlook.com>
2024-03-01 18:45:15 -0800

    Add regional score coloring

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+const us_state_regions = new Map([
+    ["WA", "US West"],
+    ["OR", "US West"],
+    ["CA", "US West"],
+    ["MT", "US West"],
+    ["ID", "US West"],
+    ["WY", "US West"],
+    ["UT", "US West"],
+    ["CO", "US West"],
+    ["NV", "US West"],
+
+    ["NM", "US Southwest"],
+    ["TX", "US Southwest"],
+    ["OK", "US Southwest"],
+    ["AZ", "US Southwest"],
+
+    ["ND", "US Midwest"],
+    ["SD", "US Midwest"],
+    ["NE", "US Midwest"],
+    ["KS", "US Midwest"],
+    ["MN", "US Midwest"],
+    ["IA", "US Midwest"],
+    ["MO", "US Midwest"],
+    ["WI", "US Midwest"],
+    ["IL", "US Midwest"],
+    ["MI", "US Midwest"],
+    ["IN", "US Midwest"],
+    ["OH", "US Midwest"],
+
+    ["AR", "US Southeast"],
+    ["LA", "US Southeast"],
+    ["KY", "US Southeast"],
+    ["TN", "US Southeast"],
+    ["MS", "US Southeast"],
+    ["WV", "US Southeast"],
+    ["MD", "US Southeast"],
+    ["VA", "US Southeast"],
+    ["NC", "US Southeast"],
+    ["SC", "US Southeast"],
+    ["GA", "US Southeast"],
+    ["AL", "US Southeast"],
+    ["FL", "US Southeast"],
+
+    ["ME", "US Northeast"],
+    ["VT", "US Northeast"],
+    ["NH", "US Northeast"],
+    ["MA", "US Northeast"],
+    ["CT", "US Northeast"],
+    ["RI", "US Northeast"],
+    ["NY", "US Northeast"],
+    ["PA", "US Northeast"],
+    ["NJ", "US Northeast"],
+    ["DE", "US Northeast"],
+
+    ["AK", "US Island"],
+    ["HI", "US Island"]
+]);
+
+
 function testGraph() {
     // Declare the chart dimensions and margins.
     const width = 640;
@@ -14,26 +73,47 @@ function testGraph() {
         .translate([width / 2, height / 2]);
 
     d3.json("world.json", function(data) {
+        const colorScale = d3.scaleLinear()
+            .domain([0, data.highestScores[3]])
+            .range(["gray", "red"]);
+
         svg.append("g")
             .selectAll("path")
             .data(data.features)
             .enter().append("path")
-                .attr("fill", "gray")
+                .attr("fill", function(d) {
+                    return colorScale(d.properties.scores[3]);
+                })
                 .attr("d", d3.geoPath()
                     .projection(projection)
                 )
-                .on("mouseover", highlightRegion)
-                .on("mouseout", unhighlightRegion);
+                // .on("mouseover", highlightRegion)
+                // .on("mouseout", unhighlightRegion);
     });
 
     function highlightRegion() {
         d3.select(this)
             .style("fill", "black")
+
+        // Show miniature graph next to the country
+        const image = svg.append("image")
+            .attr("width", 50)
+            .attr("height", 50)
+            .attr("xlink:href", "beatDAGAME.png"); // TODO: Replace image with line graph of players
+    
+        // Mousemove event listener to update image position
+        svg.on("mousemove", function() {
+            const [x, y] = d3.mouse(this);
+            image.attr("x", x + 10)
+                .attr("y", y - 10);
+        });
     }
 
     function unhighlightRegion() {
         d3.select(this)
             .style("fill", "gray")
+
+        svg.select("image").remove();
     }
 }
 
--- regionize.py
+++ regionize.py
@@ -1,4 +1,63 @@
 import json
+import math
+
+state_acronym_regions = {
+    "WA": "US West",
+    "OR": "US West",
+    "CA": "US West",
+    "MT": "US West",
+    "ID": "US West",
+    "WY": "US West",
+    "UT": "US West",
+    "CO": "US West",
+    "NV": "US West",
+
+    "NM": "US Southwest",
+    "TX": "US Southwest",
+    "OK": "US Southwest",
+    "AZ": "US Southwest",
+
+    "ND": "US Midwest",
+    "SD": "US Midwest",
+    "NE": "US Midwest",
+    "KS": "US Midwest",
+    "MN": "US Midwest",
+    "IA": "US Midwest",
+    "MO": "US Midwest",
+    "WI": "US Midwest",
+    "IL": "US Midwest",
+    "MI": "US Midwest",
+    "IN": "US Midwest",
+    "OH": "US Midwest",
+
+    "AR": "US Southeast",
+    "LA": "US Southeast",
+    "KY": "US Southeast",
+    "TN": "US Southeast",
+    "MS": "US Southeast",
+    "WV": "US Southeast",
+    "MD": "US Southeast",
+    "VA": "US Southeast",
+    "NC": "US Southeast",
+    "SC": "US Southeast",
+    "GA": "US Southeast",
+    "AL": "US Southeast",
+    "FL": "US Southeast",
+
+    "ME": "US Northeast",
+    "VT": "US Northeast",
+    "NH": "US Northeast",
+    "MA": "US Northeast",
+    "CT": "US Northeast",
+    "RI": "US Northeast",
+    "NY": "US Northeast",
+    "PA": "US Northeast",
+    "NJ": "US Northeast",
+    "DE": "US Northeast",
+
+    "AK": "US Island",
+    "HI": "US Island"
+}
 
 west = [
     "Washington",
@@ -68,6 +127,20 @@ islands = [
     "Hawaii"
 ]
 
+def get_state_region(name):
+    if name in west:
+        return "US West"
+    elif name in southwest:
+        return "US Southwest"
+    elif name in midwest:
+        return "US Midwest"
+    elif name in southeast:
+        return "US Southeast"
+    elif name in northeast:
+        return "US Northeast"
+    elif name in islands:
+        return "US Islands"
+
 out = dict()
 out["type"] = "FeatureCollection"
 out["features"] = []
@@ -105,7 +178,7 @@ southwest_feature = make_feature("US Southwest")
 midwest_feature = make_feature("US Midwest")
 southeast_feature = make_feature("US Southeast")
 northeast_feature = make_feature("US Northeast")
-islands_feature = make_feature("US Island")
+islands_feature = make_feature("US Islands")
 
 def get_feature(name):
     if name in west:
@@ -143,6 +216,71 @@ out["features"].append(southeast_feature)
 out["features"].append(northeast_feature)
 out["features"].append(islands_feature)
 
-json_out = json.dumps(out)
+feature_names = dict()
+for feature in out["features"]:
+    name = feature["properties"]["name"]
+    feature_names[name] = feature
+    feature["properties"]["scores"] = [0] * 23
+    feature["properties"]["players"] = [0] * 23
+
+players_file = open("players.json")
+players = json.load(players_file)
+
+ranks_file = open("players.json")
+ranks = json.load(ranks_file)
+
+players_file = open("players.json")
+players = json.load(players_file)
+ranks_file = open("ranks.json")
+ranks = json.load(ranks_file)
+
+def get_player_score(player_rank, lowest_rank):
+    return (lowest_rank - (player_rank - 1))
+
+out["highestScores"] = [0] * 23
+
+for ranking in ranks:
+    year = ranking["year"]
+    lowest_rank = -1
+    for player in ranking["players"]:
+        if player["rank"] > lowest_rank:
+            lowest_rank = player["rank"]
+
+    region_scores = dict()
+    region_players = dict()
+
+    for player in ranking["players"]:
+        player_location = players[player["name"]]
+        region = ""
+        if player_location["country"] == "United States":
+            region = state_acronym_regions[player_location["state"]]
+        else:
+            region = player_location["country"]
+
+        if region not in region_scores:
+            region_scores[region] = 0
+        if region not in region_players:
+            region_players[region] = []
+
+        region_scores[region] += get_player_score(player["rank"], lowest_rank)
+        region_players[region].append({
+            "name": player["name"],
+            "rank": player["rank"],
+            "state": player_location["state"]
+        })
+
+    highestScore = 0
+    for score in region_scores.values():
+        if score > highestScore:
+            highestScore = score
+    out["highestScores"][year - 2001] = highestScore
+
+    print(ranking["year"])
+    for region in region_scores.keys():
+        feature_names[region]["properties"]["scores"][year - 2001] = region_scores[region]
+        feature_names[region]["properties"]["players"][year - 2001] = region_players[region]
+        print("\t" + region + ": " + str(region_scores[region]))
+
+json_out = json.dumps(out, indent=2)
 with open("world.json", "w") as outfile:
     outfile.write(json_out)
