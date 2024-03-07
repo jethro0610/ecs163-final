30cdb132745ac3c6cb660871830c08d105150557
Jethro Peralta <jethro0610@outlook.com>
2024-03-06 18:30:18 -0800

    Cache region data globally

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let curYear = 2001;
 let highestScores;
+let regionData;
 
 function changeTime(delta) {
     const width = 800;
@@ -13,7 +14,7 @@ function changeTime(delta) {
     curYear = Math.min(Math.max(curYear, 2001), 2023);
 
     const colorScale = d3.scaleLinear()
-        .domain([0, highestScores[curYear - 2001]])
+        .domain([0, regionData.highestScores[curYear - 2001]])
         .range(["#ededed", "red"]);
 
     mapSvg.selectAll("path")
@@ -38,68 +39,64 @@ function generateLineGraph(region) {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     let scores; 
-    d3.json("world_melee_data.json", function(data) {
-        data.features.forEach(feature => {
-            if(feature.properties.name == region)
-                scores = feature.properties.scores;
-        });
-
-        const lineData = [];
-        for (let i = 0; i < scores.length; i++) {
-            const year = i + 2001;
-            if (year == 2021 || year == 2020)
-                continue;
-
-            const score = scores[i] / data.highestScores[i];
-            lineData.push({
-                score: score,
-                year: year
-            });
-        }
-
-        const x = d3.scaleLinear()
-            .domain([2001,2023])
-            .range([0, width]);
-        svg.append("g")
-            .call(
-                d3.axisBottom(x)
-                .tickFormat(d3.format("d"))
-                .tickValues([2001, 2006, 2012, 2018, 2023])
-            )
-            .attr("transform", "translate(0," + height + ")")
-    
-        const y = d3.scaleLinear()
-            .domain([0, 1])
-            .range([height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
-    
-        // Add the line
-        svg.append("path")
-            .datum(lineData)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(function(d) { return x(d.year); })
-                .y(function(d) { return y(d.score); })
-            )
-
-        svg.append("g")
-            .selectAll("dot")
-            .data(lineData)
-            .enter()
-            .append("circle")
-                .attr("cx", function(d) { return x(d.year); })
-                .attr("cy", function(d) { return y(d.score); })
-                .attr("r", 4.0)
-                .style("fill", "steelblue")
-
+    regionData.features.forEach(feature => {
+        if(feature.properties.name == region)
+            scores = feature.properties.scores;
     });
+
+    const lineData = [];
+    for (let i = 0; i < scores.length; i++) {
+        const year = i + 2001;
+        if (year == 2021 || year == 2020)
+            continue;
+
+        const score = scores[i] / regionData.highestScores[i];
+        lineData.push({
+            score: score,
+            year: year
+        });
+    }
+
+    const x = d3.scaleLinear()
+        .domain([2001,2023])
+        .range([0, width]);
+    svg.append("g")
+        .call(
+            d3.axisBottom(x)
+            .tickFormat(d3.format("d"))
+            .tickValues([2001, 2006, 2012, 2018, 2023])
+        )
+        .attr("transform", "translate(0," + height + ")")
+
+    const y = d3.scaleLinear()
+        .domain([0, 1])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+        .datum(lineData)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(function(d) { return x(d.year); })
+            .y(function(d) { return y(d.score); })
+        )
+
+    svg.append("g")
+        .selectAll("dot")
+        .data(lineData)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d) { return x(d.year); })
+            .attr("cy", function(d) { return y(d.score); })
+            .attr("r", 4.0)
+            .style("fill", "steelblue")
+
     return svg;
 }
-
-generateLineGraph("Sweden");
 
 function generateMap() {
     const width = 800;
@@ -110,46 +107,45 @@ function generateMap() {
         .attr("width", width)
         .attr("height", height);
 
-    d3.json("world_melee_data.json", function(data) {
-        // This is hacky and I hate it, but whatever lol
-        highestScores = data.highestScores;
+    const projection = d3.geoMercator()
+        .scale(150)
+        .center([-50, 20])
+        .translate([width / 2, height / 2]);
 
-        const projection = d3.geoMercator()
-            .scale(150)
-            .center([-50, 20])
-            .translate([width / 2, height / 2]);
+    const zoom = d3.zoom()
+        .scaleExtent([1, 10]) // Zoom levels
+        .on("zoom", zoomed);
 
-        const zoom = d3.zoom()
-            .scaleExtent([1, 10]) // Zoom levels
-            .on("zoom", zoomed);
+    svg.call(zoom); // Calling the zoom function
 
-        svg.call(zoom); // Calling the zoom function
+    // Function to handle zooming
+    function zoomed() {
+        // We need to figure out how to have the map repeat itself
+        // I read online that you can have two projection next to
+        // each other. Maye that works?
+        g.attr("transform", d3.event.transform);
+    }
 
-        // Function to handle zooming
-        function zoomed() {
-            // We need to figure out how to have the map repeat itself
-            // I read online that you can have two projection next to
-            // each other. Maye that works?
-            g.attr("transform", d3.event.transform);
-        }
+    const colorScale = d3.scaleLinear()
+        .domain([0, regionData.highestScores[curYear - 2001]])
+        .range(["#ededed", "red"]);
 
-        const colorScale = d3.scaleLinear()
-            .domain([0, highestScores[curYear - 2001]])
-            .range(["#ededed", "red"]);
-
-        const g = svg.append("g");
-        g.selectAll("path")
-            .data(data.features)
-            .enter().append("path")
-                .attr("fill", function(d) {
-                    return colorScale(d.properties.scores[curYear - 2001]);
-                })
-                .attr("d", d3.geoPath()
-                    .projection(projection)
-                )
-    });
+    const g = svg.append("g");
+    g.selectAll("path")
+        .data(regionData.features)
+        .enter().append("path")
+            .attr("fill", function(d) {
+                return colorScale(d.properties.scores[curYear - 2001]);
+            })
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
 
     return svg;
 }
 
-generateMap();
+d3.json("world_melee_data.json", function(data) {
+    regionData = data;
+    generateMap();
+    generateLineGraph("Sweden");
+});
