0f17b4a64b3a1f3b2481ec18ddfe211ac4d5f7ef
Jethro Peralta <jethro0610@outlook.com>
2024-03-06 18:38:02 -0800

    Separate map and graph to files. Globalize region data

---
+++ map.js
@@ -0,0 +1,67 @@
+let curYear = 2001;
+const mapWidth = 800;
+const mapHeight = 600;
+
+function changeMapTime(delta) {
+    const mapSvg = d3.select("#map")
+        .attr("width", mapWidth)
+        .attr("height", mapHeight);
+
+    curYear += delta;
+    curYear = Math.min(Math.max(curYear, 2001), 2023);
+
+    const colorScale = d3.scaleLinear()
+        .domain([0, regionData.highestScores[curYear - 2001]])
+        .range(["#ededed", "red"]);
+
+    mapSvg.selectAll("path")
+        .transition()
+        .attr("fill", function(d) {
+            return colorScale(d.properties.scores[curYear - 2001]);
+        })
+
+    document.getElementById("year").innerHTML = curYear;
+}
+
+function generateMap() {
+    // Create the SVG container.
+    const svg = d3.select("#map")
+        .attr("width", mapWidth)
+        .attr("height", mapHeight);
+
+    const projection = d3.geoMercator()
+        .scale(150)
+        .center([-50, 20])
+        .translate([mapWidth / 2, mapHeight / 2]);
+
+    const zoom = d3.zoom()
+        .scaleExtent([1, 10]) // Zoom levels
+        .on("zoom", zoomed);
+
+    svg.call(zoom); // Calling the zoom function
+
+    // Function to handle zooming
+    function zoomed() {
+        // We need to figure out how to have the map repeat itself
+        // I read online that you can have two projection next to
+        // each other. Maye that works?
+        g.attr("transform", d3.event.transform);
+    }
+
+    const colorScale = d3.scaleLinear()
+        .domain([0, regionData.highestScores[curYear - 2001]])
+        .range(["#ededed", "red"]);
+
+    const g = svg.append("g");
+    g.selectAll("path")
+        .data(regionData.features)
+        .enter().append("path")
+            .attr("fill", function(d) {
+                return colorScale(d.properties.scores[curYear - 2001]);
+            })
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+
+    return svg;
+}
