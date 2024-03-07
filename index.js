c8dbbf3c5812fa8997ac9eceeee21803da0c01ab
Jethro Peralta <jethro0610@outlook.com>
2024-03-06 18:27:24 -0800

    Move line graph to main pages. Add dots to lines

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let curYear = 2001;
-const width = 640;
-const height = 400;
 let highestScores;
 
 function changeTime(delta) {
+    const width = 800;
+    const height = 600;
+
     const mapSvg = d3.select("#map")
         .attr("width", width)
         .attr("height", height);
@@ -24,7 +25,86 @@ function changeTime(delta) {
     document.getElementById("year").innerHTML = curYear;
 }
 
+function generateLineGraph(region) {
+    const margin = {top: 30, right: 30, bottom: 30, left: 30}
+    const width = 500 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    // Create the SVG container.
+    var svg = d3.select("#left-panel").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let scores; 
+    d3.json("world_melee_data.json", function(data) {
+        data.features.forEach(feature => {
+            if(feature.properties.name == region)
+                scores = feature.properties.scores;
+        });
+
+        const lineData = [];
+        for (let i = 0; i < scores.length; i++) {
+            const year = i + 2001;
+            if (year == 2021 || year == 2020)
+                continue;
+
+            const score = scores[i] / data.highestScores[i];
+            lineData.push({
+                score: score,
+                year: year
+            });
+        }
+
+        const x = d3.scaleLinear()
+            .domain([2001,2023])
+            .range([0, width]);
+        svg.append("g")
+            .call(
+                d3.axisBottom(x)
+                .tickFormat(d3.format("d"))
+                .tickValues([2001, 2006, 2012, 2018, 2023])
+            )
+            .attr("transform", "translate(0," + height + ")")
+    
+        const y = d3.scaleLinear()
+            .domain([0, 1])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+    
+        // Add the line
+        svg.append("path")
+            .datum(lineData)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function(d) { return x(d.year); })
+                .y(function(d) { return y(d.score); })
+            )
+
+        svg.append("g")
+            .selectAll("dot")
+            .data(lineData)
+            .enter()
+            .append("circle")
+                .attr("cx", function(d) { return x(d.year); })
+                .attr("cy", function(d) { return y(d.score); })
+                .attr("r", 4.0)
+                .style("fill", "steelblue")
+
+    });
+    return svg;
+}
+
+generateLineGraph("Sweden");
+
 function generateMap() {
+    const width = 800;
+    const height = 600;
+
     // Create the SVG container.
     const svg = d3.select("#map")
         .attr("width", width)
